8eb7941f4a44374b2f6a3ddfabe0f0acff9994e9
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-19 20:04:46 +0000

    adding and removing products to/from basket done + disabling buttons

--- app.js
+++ app.js
@@ -1,35 +1,31 @@
 
-class App extends React.Component{
-
-
-    render(){
-        <div>{}</div>
-    }
+class App extends React.Component {
+state = {
+    productsAvailable: 7,
+    shoppingCart: 1,
 }
 
+handleAddToCart = () =>{ 
+    this.setState({
+        shoppingCart: this.state.shoppingCart + 1,
+    })
 
+}
+handleRemoveFromCart = () =>{
+    this.setState({
+        shoppingCart: this.state.shoppingCart - 1,
+    })
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    render() {
+        return (
+            <>
+                <button disabled={this.state.shoppingCart ? false: true} onClick={this.handleRemoveFromCart}>-</button>
+                <span> {this.state.shoppingCart} </span>
+                <button disabled={(this.state.shoppingCart === this.state.productsAvailable) ? true: false} onClick={this.handleAddToCart}>+</button>
+            </>)
+    }
+}
 
 
 
