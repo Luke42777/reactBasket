17aac22d3cb1fb32070dba0a44d47d3f7bd95d1f
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-19 18:55:06 +0000

    structure done, css styles

---
+++ app.js
@@ -0,0 +1,37 @@
+
+class App extends React.Component{
+
+
+    render(){
+        <div>{}</div>
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById("root"));
