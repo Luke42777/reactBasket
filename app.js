18f4f84d879f5c2eb71fed05e5ac5d7b2bc6b692
Luke42777 <55854062+Luke42777@users.noreply.github.com>
2022-01-20 17:05:59 +0000

    conditional rendering-Buy button

--- app.js
+++ app.js
@@ -16,13 +16,19 @@ handleRemoveFromCart = () =>{
         shoppingCart: this.state.shoppingCart - 1,
     })
 }
-
+handleBuy = () => {
+    this.setState({
+        productsAvailable: this.state.productsAvailable - this.state.shoppingCart,
+        shoppingCart: 0
+    })
+}
     render() {
         return (
             <>
                 <button disabled={this.state.shoppingCart ? false: true} onClick={this.handleRemoveFromCart}>-</button>
                 <span> {this.state.shoppingCart} </span>
                 <button disabled={(this.state.shoppingCart === this.state.productsAvailable) ? true: false} onClick={this.handleAddToCart}>+</button>
+                {this.state.shoppingCart > 0 && <button className="buy" onClick={this.handleBuy}>Buy</button>}
             </>)
     }
 }
